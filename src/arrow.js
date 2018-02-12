79a8ef7f407eed20ecfa77a0251ab5b50823d5e2
kramduckner <buckner.a.mark@gmail.com>
2018-02-12 16:05:22 -0700

    Update arrow.js

--- src/arrow.js
+++ src/arrow.js
@@ -13,6 +13,7 @@ barf(function(data){
 });
 
 //convert the callback function above to use arrow notation. 
+//then, modify the code to work - you'll get an error on line 8 saying "data is not defined" or somesuch
 
 //Last part - notice how data gets passed through a callback. Not so magical
 //anymore right? Add a property to the object catData and log it in the
